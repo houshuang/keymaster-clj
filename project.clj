093f6f9591e859c46e52f276635438b402b0fde5
micmarsh <michael.marsh42@gmail.com>
2013-04-30 22:41:21 -0400

    included jna

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.github.tulskiy/jkeymaster "1.1"]]
+                 [com.github.tulskiy/jkeymaster "1.1"]
+                 [jna "3.2.7"]]
   :main keymaster.core)
