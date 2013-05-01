57cc799a0a7b4bf4a1bfbdd7a8fb37f0107898f7
Stian Håklev <shaklev@gmail.com>
2013-05-01 11:17:14 -0400

    Updating jna dependency based on Maven availability

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.github.tulskiy/jkeymaster "1.1"]
-                 [jna "3.2.7"]])
+                 [net.java.dev.jna/jna "3.5.2"]])
